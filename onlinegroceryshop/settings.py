26a0b47ae75b3e8328a7c4e9bd2fec5138023c3d
Sai Kishore Bhadragiri <kishorekittu119@gmail.com>
2025-04-10 19:06:28 -0400

    Added all image assets and updated settings

--- onlinegroceryshop/settings.py
+++ onlinegroceryshop/settings.py
@@ -112,6 +112,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 STATICFILES_DIRS=[
 STATIC_DIR,
  ]
